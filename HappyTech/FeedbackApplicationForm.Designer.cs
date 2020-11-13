f6508cd8b891dfe917694e5eae304776ddcb972a
Roan O'Connor-Davy <roanc.oconnor@outlook.com>
2020-11-13 19:10:14 +0000

    Add project files.

---
+++ HappyTech/FeedbackApplicationForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace HappyTech
+{
+    partial class FeedbackApplicationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.navigationPanel = new System.Windows.Forms.Panel();
+            this.timeDateTable = new System.Windows.Forms.TableLayoutPanel();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.navigationPanel.SuspendLayout();
+            this.timeDateTable.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // navigationPanel
+            // 
+            this.navigationPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.navigationPanel.Controls.Add(this.timeDateTable);
+            this.navigationPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.navigationPanel.Location = new System.Drawing.Point(0, 0);
+            this.navigationPanel.Name = "navigationPanel";
+            this.navigationPanel.Size = new System.Drawing.Size(215, 561);
+            this.navigationPanel.TabIndex = 0;
+            // 
+            // timeDateTable
+            // 
+            this.timeDateTable.ColumnCount = 1;
+            this.timeDateTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.timeDateTable.Controls.Add(this.dateLabel, 0, 1);
+            this.timeDateTable.Controls.Add(this.timeLabel, 0, 0);
+            this.timeDateTable.Location = new System.Drawing.Point(4, 4);
+            this.timeDateTable.Name = "timeDateTable";
+            this.timeDateTable.RowCount = 2;
+            this.timeDateTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.timeDateTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.timeDateTable.Size = new System.Drawing.Size(208, 48);
+            this.timeDateTable.TabIndex = 0;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.timeLabel.ForeColor = System.Drawing.Color.White;
+            this.timeLabel.Location = new System.Drawing.Point(3, 0);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(202, 24);
+            this.timeLabel.TabIndex = 0;
+            this.timeLabel.Text = "Time";
+            this.timeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dateLabel.ForeColor = System.Drawing.Color.White;
+            this.dateLabel.Location = new System.Drawing.Point(3, 24);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(202, 24);
+            this.dateLabel.TabIndex = 1;
+            this.dateLabel.Text = "Date";
+            this.dateLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FeedbackApplicationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 561);
+            this.Controls.Add(this.navigationPanel);
+            this.MinimumSize = new System.Drawing.Size(1200, 600);
+            this.Name = "FeedbackApplicationForm";
+            this.Text = "HappyTech Feedback Application";
+            this.navigationPanel.ResumeLayout(false);
+            this.timeDateTable.ResumeLayout(false);
+            this.timeDateTable.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel navigationPanel;
+        private System.Windows.Forms.TableLayoutPanel timeDateTable;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.Label timeLabel;
+    }
+}
+
